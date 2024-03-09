0591f66ca61762081bb7e395da691642fffe68dc
Mike Goebel <mike@mg-develop.de>
2024-03-09 23:08:01 +0100

    test

--- src/Text/Text.tsx
+++ src/Text/Text.tsx
@@ -1,31 +1,10 @@
-import React from 'react';
 import cn from 'classnames';
-
-import colors from './Text.colors.module.scss';
-import themes from './Text.themes.module.scss';
-
-export type TextThemes = keyof typeof themes;
-export type TextColors = keyof typeof colors;
+import React from 'react';
 
 interface Props {
   className?: string;
-  theme?: keyof typeof themes;
-  color?: keyof typeof colors;
 }
 
-export const Text: React.FC<Props> = ({
-  className,
-  theme = 'h1',
-  color,
-  children
-}) => (
-  <span
-    className={cn(
-      themes[theme],
-      color && colors[color],
-      className
-    )}
-  >
-    {children}
-  </span>
+export const Text: React.FC<Props> = ({ className, children }) => (
+  <span className={cn(className)}>{children}</span>
 );
